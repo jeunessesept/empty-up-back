219fd298f27ab65a2bf63fc357df5aa064b6a0a6
jeunessesept <b.jiacinto@gmail.com>
2023-03-16 12:09:22 +0100

    controllers/like toLike redone, with a UPSERT query

--- controllers/likes.mjs
+++ controllers/likes.mjs
@@ -3,36 +3,31 @@ import { pool } from "../models/dbPool.mjs";
 export const toLike = async (req, res) => {
   const user_id = req.userId;
   const building_id = req.params.buildingId;
+
+  if (!user_id || !building_id) {
+    return res.status(400).json({ error: "bad request" });
+  }
   try {
-    const queryCheck = await pool.query(
-      "SELECT * FROM like_per_building WHERE building_id = $1 AND user_id = $2",
+    const query = await pool.query(
+      "INSERT INTO like_per_building " +
+        '(building_id, user_id, "like") ' +
+        "VALUES ($1, $2, true) " +
+        "ON CONFLICT (building_id, user_id) DO UPDATE SET " +
+        '"like" = NOT EXCLUDED.like',
       [building_id, user_id]
     );
-    if (queryCheck.rows.length === 0) {
-      await pool.query(
-        'INSERT INTO like_per_building (building_id, user_id, "like") VALUES ($1, $2, true)',
-        [building_id, user_id]
-      );
-      return res.status(200).json({ message: "like inserted successfully" });
-    } else if (queryCheck.rows[0].like === true) {
-      await pool.query(
-        'UPDATE like_per_building SET "like" = false WHERE building_id = $1 AND user_id = $2',
-        [building_id, user_id]
-      );
-      return res.status(200).json({ message: "like updated to false" });
-    } else if (queryCheck.rows[0].like === false) {
-      await pool.query(
-        'UPDATE like_per_building SET "like" = true WHERE building_id = $1 AND user_id = $2',
-        [building_id, user_id]
-      );
-      return res.status(200).json({ message: "like updated to true" });
+    let message;
+    if (query.rowCount === 1) {
+      message = "Building liked successfully";
+    } else {
+      message = `Building like status updated to ${query.rows[0].like}`;
     }
+    return res.status(200).json({ message });
   } catch (error) {
     console.error(error);
     return res.status(500).send({ error: "internal server error" });
   }
-}
-
+};
 
 export const getLike = async (req, res) => {
   const user_id = req.userId;
@@ -45,25 +40,25 @@ export const getLike = async (req, res) => {
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 export const getLikeWithBuildingsInfos = async (req, res) => {
-    const user_id = req.userId;
-    try{
-       const query = await pool.query(
-        'SELECT b.adress AS building_adress, ' +
-        'b.zipcode AS building_zipcode, ' +
-        'b.city AS building_city, ' +
-        'b.type AS building_type, ' +
-        'b.initial_image AS building_image, ' +
-        'l.like ' +
-        'FROM like_per_building l ' +
-        'JOIN buildings b ON l.building_id = b.id ' + 
-        'WHERE l.user_id = $1',
-        [user_id]
-       );
-    return res.status(200).json({info: query.rows})
-    }catch(error){
-        console.error(error)
-    }
-}
+  const user_id = req.userId;
+  try {
+    const query = await pool.query(
+      "SELECT b.adress AS building_adress, " +
+        "b.zipcode AS building_zipcode, " +
+        "b.city AS building_city, " +
+        "b.type AS building_type, " +
+        "b.initial_image AS building_image, " +
+        "l.like " +
+        "FROM like_per_building l " +
+        "JOIN buildings b ON l.building_id = b.id " +
+        "WHERE l.user_id = $1",
+      [user_id]
+    );
+    return res.status(200).json({ info: query.rows });
+  } catch (error) {
+    console.error(error);
+  }
+};
